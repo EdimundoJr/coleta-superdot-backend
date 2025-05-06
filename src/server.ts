087f2548d11c0b7e78d17db3ee0a8d27810d047a
Edimundo Junior <juniorconds@gmail.com>
2025-05-06 15:57:40 -0300

    vercel configs

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 import env from "./util/validateEnv";
 import app from "./app";
 
-// Conecta ao MongoDB e inicia o servidor
 const startServer = async () => {
     try {
         await mongoose.connect(env.MONGO_CONNECTION_STRING, { dbName: "superdot" });
@@ -12,12 +11,11 @@ const startServer = async () => {
             console.log("Server running on port: " + env.PORT);
         });
 
-        // Exporta o servidor para a Vercel (obrigatório para serverless)
         module.exports = server;
 
     } catch (error) {
         console.error("Failed to connect to MongoDB:", error);
-        process.exit(1); // Encerra o processo em caso de erro
+        process.exit(1);
     }
 };
 
