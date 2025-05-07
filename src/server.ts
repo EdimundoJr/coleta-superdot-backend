01d85f77cbc92ec8cb83fde8df5ffd4f74943f48
Edimundo Junior <juniorconds@gmail.com>
2025-05-07 15:16:52 -0300

    Render config

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import mongoose from "mongoose";
 import env from "./util/validateEnv";
 import app from "./app";
 
+const port = process.env.PORT || 3000;
+
 mongoose.connect(env.MONGO_CONNECTION_STRING, {
     dbName: "superdot"
 }).then(() => {
     console.log("Mongoose Connected");
-    app.listen(process.env.PORT, () => {
-        console.log("Server running on port: " + process.env.PORT);
+    app.listen(port, () => {
+        console.log("Server running on port: " + port);
     });
 }).catch(err => console.error(err)); 
