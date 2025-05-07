6d074cc21c93e26d595d3be2ba03cfc52d2123d2
Edimundo Junior <juniorconds@gmail.com>
2025-05-07 16:48:55 -0300

    Render Config

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 import mongoose from "mongoose";
 import env from "./util/validateEnv";
 import app from "./app";
@@ -11,4 +13,4 @@ mongoose.connect(env.MONGO_CONNECTION_STRING, {
     app.listen(port, () => {
         console.log("Server running on port: " + port);
     });
-}).catch(err => console.error(err)); 
+}).catch(err => console.error(err));
